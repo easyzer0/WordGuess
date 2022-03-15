d7b81f0942c5a381a1a8dd826f492b5b57797356
Obi Enechionyia <obi@MacBook-Pro.local>
2022-03-15 20:54:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Word Guess Game
+
+This is a Python word guessing game that allows users 6 chances to guess a randomly chosen word from a list of common 5 letter words. If any of the letters in the user's guess are correct, they are shown to the user in their correct position.
---
+++ word_guess.py
@@ -0,0 +1,62 @@
+import random
+
+def word():
+    with open("words.txt", "r") as words:
+        lines = words.readlines()
+    wordList = []
+    for word in lines:
+        wordList.append(word.strip("\n"))
+    
+    gameWord = random.choice(wordList)
+    return gameWord
+
+def userChoice():
+    count = 0
+    gameWord = word()
+    letterList = list(gameWord)
+    userLetters = []
+    letterBlanks = "*****"
+    while True:
+        try:
+            userWord = input("What's your guess?: ")
+            if userWord == "exit":
+                exit()
+            assert len(userWord) == 5
+        except AssertionError:
+            print("Word must be 5 letters long!")
+            continue
+        if userWord == gameWord:
+            print("Correct!")
+            break
+        else:
+            if count < 3:
+                print(f"Wrong. {4 - count} tries remaining.")
+            if count == 3:
+                print("Wrong. 1 try remaining.")
+
+            for i in letterList:
+                if i in userWord:
+                    if i not in userLetters:
+                        userLetters.append(i)
+                    letterBlanks.replace("*", i)
+            for i in userLetters:
+                letterIndex = gameWord.index(i)
+                letterBlanks = replaceBlanks(letterBlanks, letterIndex, i.upper())
+            if count < 4:
+                print(letterBlanks)
+        count += 1
+        if count > 4:
+            print(f"Wrong. You ran out of tries! The answer was {gameWord.upper()}!")
+            break
+    playAgain = input("Play again? (y/n): ")
+    if playAgain == "y":
+        userChoice()
+    else:
+        quit()
+
+
+def replaceBlanks(blank, index, replacement):
+    return "%s%s%s"%(blank[:index], replacement, blank[index+1:])
+
+
+userChoice()
